bfeba35e4e45d080f12193fb6c0687070dc66909
Cong Nguyen <nqc0801@gmail.com>
2022-10-17 21:12:05 +0700

    done

--- src/components/user/SearchUser.jsx
+++ src/components/user/SearchUser.jsx
@@ -3,23 +3,24 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ICONSEARCH } from '../../Icon';
 function SearchUser(props) {
-
-
-const [value,setValue]=useState("")
-    const navi =useNavigate()
-const func= (event) =>{
-    if(event.keyCode == 13){
-        navi({
+    const [value, setValue] = useState("")
+    const navi = useNavigate()
+    const func = (event) => {
+        if (event.keyCode === 13) {
+            funcs()
+        }
+    }
+    const funcs = () => {
+        value.length > 0 ? navi({
             pathname: '/search',
             search: `&name_like=${value}`,
-          })
-        
+        }) :
+            navi("/")
     }
-}
     return (
         <div className='search-header'>
-            <input type="text" className="form-control"onKeyDown={func} onChange={(e)=>{setValue(e.target.value)}} placeholder="Search..."></input>
-            <span className="ICONSEARCH">
+            <input type="text" className="form-control" onKeyDown={func} onChange={(e) => { setValue(e.target.value) }} placeholder="Search..."></input>
+            <span onClick={funcs} className="ICONSEARCH">
                 <i className={ICONSEARCH} ></i>
             </span>
         </div>
