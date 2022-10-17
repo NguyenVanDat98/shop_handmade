b8c7ea970c7a0efa40a8e39b738dd2c5e5e35907
Dat <nguyenvom98@gmail.com>
2022-10-17 18:28:35 +0700

    update

--- src/components/admin/MainDashboard.jsx
+++ src/components/admin/MainDashboard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PieChartRanger from '../Chart/PieChartRanger';
 import {ItemView,ChartBar,PieChart} from './../../components/';
 
-const MainDashboard = props => {
+const  MainDashboard = props => {
     const itemView= ["Order","User","Earning"]
     return (
         <div className='main-dashboard'>
-            <div className='d-flex flex-lg-column justify-content-around flex mt-3 mb-3 '>
+            <div className='d-flex flex-lg-column flex-md-row justify-content-around flex mt-3 mb-3 '>
                  {[...new Array(3)].map((a,i)=><ItemView total={i} key={i}>{itemView[i]} </ItemView>)}
 
             </div>
