2b1353268647efe0618db3c212f9331a0b782bc3
Dat <nguyenvom98@gmail.com>
2022-10-19 14:02:56 +0700

    COMMIT

--- src/components/admin/SearchAd.jsx
+++ src/components/admin/SearchAd.jsx
@@ -9,31 +9,32 @@ const ALL_DATA = useSelector(_=>_.adminData)
 
 const handleSearch =()=>{
     let outPut =[]
+    const Check=()=>{
+        return{
+            validArr: ( _, keyy,key)=>{
+                let temp= keyy && keyy.toString()                    
+                    if( keyy&&temp.includes(typing)){
+                        outPut.push({address: key,data: _})
+                     return
+                    }
+            },
+
+        }
+    }
     if(typing.length){
       for (const key in ALL_DATA) {
     if (Object.hasOwnProperty.call(ALL_DATA, key)) {   
       const valueFinal = Array.isArray(ALL_DATA[key]) && ALL_DATA[key].filter((_,i)=>{
           for (const keyy in _) {
               if (Object.hasOwnProperty.call(_, keyy) && !keyy.includes("id") && !keyy.includes("img") ) {
-                //   console.log(keyy);
                 if(!Array.isArray(_[keyy])){
-                    let temp= _[keyy].toString()                    
-                    if(temp.includes(typing)){
-                        outPut.push({address: key,data: _})
-                     return
-                    }
+                    Check().validArr(_,_[keyy],key)
                 }else{
                     for (const value of _[keyy]) {
                         for (const keyy in value) {
                             if (Object.hasOwnProperty.call(value, keyy) && !keyy.includes("id") && !keyy.includes("img")) {
-                                // console.log(keyy);
                                 if(!Array.isArray(value[keyy]) ){
-                                    let temp= value[keyy].toString()    
-                                    if(temp.includes(typing)){
-                                        // console.log(value)
-                                        outPut.push({address: key,data: value})
-                                     return
-                                    }
+                                    Check().validArr(value,_[keyy],key)
                                 }
                             }
                         }
@@ -41,7 +42,15 @@ const handleSearch =()=>{
                 }
             }
         }       
-      })     
+      })  
+      if(!Array.isArray(ALL_DATA[key])){
+        for (const key in ALL_DATA) {
+            if (Object.hasOwnProperty.call(ALL_DATA, key) && !key.includes("id") && !key.includes("img")) {
+                if(!Array.isArray(ALL_DATA[key])){
+                    console.log(ALL_DATA[key])}
+            }
+        }
+      }
     }
    }   
     }
